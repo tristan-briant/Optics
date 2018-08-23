7bdd52291da4c9a75ecbca70487a73568449f5c6
tristan-briant <tristan.briant@upmc.fr>
2018-08-23 18:29:38 +0200

    LightRay added

---
+++ optics/Assets/LightSource.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSource : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < 10; i++) {
+
+            GameObject ray = new GameObject("Ray");
+
+            LightRay r =ray.AddComponent<LightRay>();
+            r.StartPosition = new Vector3(0.0f, 0, 0);
+            r.Length = 5.0f;
+            r.Width = 0.0f;
+            r.Divergence = 2 * Mathf.PI / 100;
+            r.Direction = new Vector3(Mathf.Cos(2*Mathf.PI / 100 * i), Mathf.Sin(2 * Mathf.PI / 100 * i), 0);
+            r.Col = new Color(1, 1, 1, 0.5f);
+            r.intensity = 0.01f;
+
+            r.Draw();
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ optics/Assets/Scripts/LightRay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRay : MonoBehaviour {
+
+    public Vector3 StartPosition;
+    public Vector3 Direction;
+    public float Length;
+    public Color Col;
+    public float Width;
+    public float Divergence;
+    public float intensity=1;
+
+    LineRenderer lr;
+
+    public void Draw() {
+        lr = gameObject.AddComponent<LineRenderer>();
+
+        lr.material = new Material(Shader.Find("Particles/Additive"));
+
+        lr.SetPosition(0, StartPosition);
+        lr.SetPosition(1, StartPosition + Length * Direction);
+
+        lr.startWidth = Width;
+        lr.endWidth = Width + Divergence*Length;
+
+        Color c = Col;
+        c.a = intensity / Width;
+        lr.startColor = c;
+       
+        c.a = intensity / (Width + Divergence * Length);
+        lr.endColor = c;
+        
+    }
+	
+
+
+}
